81f3342aec33d2a87708ae65d2b6196d94b7ff49
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-15 10:06:23 -0400

    created outline for HTML, started js loop for timer

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// need timer
+
+for (time = 60; time > 0; time--);
+//number in window changes to = count
+//transition time = 1 sec
+
+//need to ask questions with multiple choice answers
+
+//need to remove time when wrong answer is selected
+
+//need to make game end when all correct answers are selected or time runs out
+// do i have to turn count into a variable to be referenced?
+// if statement that says if count = 0 or all answers are correct
+
+if (time === 0) {
+  console.log("Game Over! Try again!");
+} else if ((game = "won")) {
+  console.log("Yay, you won!");
+}
