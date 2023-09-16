07974b5441764983e26022a0687eca495df6ca11
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-15 21:22:03 -0400

    created the timer, buttons, and question skeleton

--- js/script.js
+++ js/script.js
@@ -1,6 +1,52 @@
 // need timer
+var outputEl = document.querySelector("#output");
+var startBtn = document.querySelector("#start");
+var choicesDiv = document.querySelector(".choices");
+var questionIndex = 0;
+var time = 60;
 
-for (time = 60; time > 0; time--);
+function start() {
+  showQuestion();
+  var timer = setInterval(function () {
+    time--;
+    if (time < 0) {
+      time = 0;
+    }
+    outputEl.innerText = time;
+
+    console.log(time);
+
+    if (time <= 0) {
+      clearInterval();
+    }
+  }, 1000);
+}
+
+function makeChoice(eventObj) {
+  var button = eventObj.target;
+  var id = button.id;
+
+  // if button is not equal to "correct" lose 10 sec
+  if (id !== "correct") {
+    time -= 10;
+
+    questionIndex++;
+
+    if (questionIndex > questions.length) {
+      //they have won!
+    } else {
+      showQuestion();
+    }
+  }
+}
+
+function showQuestion() {
+  //show current question based on index
+}
+
+startBtn.addEventListener("click", start);
+choicesDiv.addEventListener("click", makeChoice);
+
 //number in window changes to = count
 //transition time = 1 sec
 
