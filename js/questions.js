e8f849bbaf82485744d3d43bc1e23a4378294826
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-17 22:26:34 -0400

    added 5 questions, fixed bug with timer, added checkAnswer function

---
+++ js/questions.js
@@ -0,0 +1,53 @@
+// var questionIndex = [
+//   "question1",
+//   "question2",
+//   "question2",
+//   "question4",
+//   "question5",
+// ];
+
+var questions = [
+  {
+    text: "HTML is used for:",
+    choices: [
+      "Styling web pages",
+      "Adding interactivity to web pages",
+      "Structuring the content of web pages",
+      "None of the above",
+    ],
+    correctAnswer: "Structuring the content of web pages",
+  },
+  {
+    text: "Which of the following is NOT a valid CSS selector?",
+    choices: [
+      ".class-selector",
+      "#id-selector",
+      "*-selector",
+      "element-selector",
+    ],
+    correctAnswer: "*-selector",
+  },
+  // Which of the following is NOT a valid CSS selector?
+  // a) .class-selector
+  // b) #id-selector
+  // c) *-selector
+  // d) element-selector
+
+  // What is the correct syntax for creating a JavaScript function?
+  // a) function myFunction() {}
+  // b) myFunction = function() {}
+  // c) myFunction() {}
+  // d) function = myFunction() {}
+
+  // How do you add a comment in HTML?
+  // a)
+  // b) // This is a comment
+  // c) /* This is a comment */
+  // d) # This is a comment
+
+  // Which of the following is NOT a JavaScript data type?
+  // a) String
+  // b) Boolean
+  // c) Array
+  // d) Integer
+];
--- js/script.js
+++ js/script.js
@@ -1,20 +1,77 @@
 // need timer
-var outputEl = document.querySelector("#output");
+var questionWrap = document.querySelector("#question-wrap");
+var timeEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
 var choicesDiv = document.querySelector(".choices");
+var questionTextEl = document.querySelector("#question-wrap h3");
 var questionIndex = 0;
+var choiceAlert = document.querySelector("#test");
 var time = 60;
+var score = 0;
+var question;
 
-function start() {
+function showQuestion() {
+  var question = questions[questionIndex];
+
+  questionTextEl.innerText = question.text;
+
+  choicesDiv.innerHTML = " ";
+
+  question.choices.forEach(function (choice) {
+    choicesDiv.insertAdjacentHTML(
+      "beforeend",
+      "<button>" + choice + "</button>"
+    );
+  });
+}
+
+// get the button text using the event object target property and innerText
+// Target is the element that the user clicked on (button)
+// Check if the button is equal to question.correctAnswer
+
+function checkAnswer(e) {
+  var button = e.target;
+  var buttonText = button.innerText;
+
+  if (buttonText === questions[questionIndex].correctAnswer) {
+    choiceAlert.innerText = "Correct!";
+  } else {
+    choiceAlert.innerText = "Wrong!";
+    time -= 10;
+  }
+
+  choiceAlert.classList.remove("hide");
+
+  setTimeout(function () {
+    choiceAlert.classList.add("hide");
+  }, 1500);
+
+  questionIndex++;
+
+  if (questionIndex === questions.length) {
+    return showHighscores();
+  }
+
   showQuestion();
+}
+
+// Starts game, makes assets visible, runs show question function, starts timer
+
+function startGame() {
+  questionWrap.classList.remove("hide");
+  timeEl.classList.remove("hide");
+  startBtn.classList.add("hide");
+
+  showQuestion();
+
   var timer = setInterval(function () {
     time--;
     if (time < 0) {
       time = 0;
     }
-    outputEl.innerText = time;
+    timeEl.innerText = time;
 
-    console.log(time);
+    // console.log(time);
 
     if (time <= 0) {
       clearInterval();
@@ -22,44 +79,5 @@ function start() {
   }, 1000);
 }
 
-function makeChoice(eventObj) {
-  var button = eventObj.target;
-  var id = button.id;
-
-  // if button is not equal to "correct" lose 10 sec
-  if (id !== "correct") {
-    time -= 10;
-
-    questionIndex++;
-
-    if (questionIndex > questions.length) {
-      //they have won!
-    } else {
-      showQuestion();
-    }
-  }
-}
-
-function showQuestion() {
-  //show current question based on index
-}
-
-startBtn.addEventListener("click", start);
-choicesDiv.addEventListener("click", makeChoice);
-
-//number in window changes to = count
-//transition time = 1 sec
-
-//need to ask questions with multiple choice answers
-
-//need to remove time when wrong answer is selected
-
-//need to make game end when all correct answers are selected or time runs out
-// do i have to turn count into a variable to be referenced?
-// if statement that says if count = 0 or all answers are correct
-
-if (time === 0) {
-  console.log("Game Over! Try again!");
-} else if ((game = "won")) {
-  console.log("Yay, you won!");
-}
+startBtn.addEventListener("click", startGame);
+choicesDiv.addEventListener("click", checkAnswer);
